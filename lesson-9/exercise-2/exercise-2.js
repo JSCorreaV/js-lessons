ed48ac72122b1d4b15f9beca0d86ddd1299ae887
juan sebastian correa <j.s.correa@videoamp.com>
2023-06-06 22:39:00 -0500

    second exercise solution

--- lesson-9/exercise-2/exercise-2.js
+++ lesson-9/exercise-2/exercise-2.js
@@ -4,6 +4,15 @@ Function `findTriangleType()` finds the type of the triangle (Equilateral, Isosc
 */
 
 function findTriangleType(){
+    first = Number(document.getElementById("firstNumber").value);
+    second = Number(document.getElementById("secondNumber").value);
+    third= Number(document.getElementById("thirdNumber").value);
 
-    //print in html the type of triangle
+    if (first == second && first == third){
+            document.getElementById("triangleType").value = "equilateral";
+    }else if (third == second || second == third || first == third){
+            document.getElementById("triangleType").value = "Isosceles";
+    } else {
+        document.getElementById("triangleType").value = "Scalene";
+    }
 }
