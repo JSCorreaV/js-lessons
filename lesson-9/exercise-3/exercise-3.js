3b5078cce38372390d59724b3f5be7ca82ae2ffd
juan sebastian correa <j.s.correa@videoamp.com>
2023-06-06 20:37:13 -0500

    adding exercise 3

---
+++ lesson-9/exercise-3/exercise-3.js
@@ -0,0 +1,13 @@
+/*
+Function `findDaysInMonth()` finds the number of days in a given month of a year.
+
+-If month is outside the range of 1 and 12 print “Invalid month”.
+-If month is equal to 2 ie, February print “29 days” if leap year else print “28 days” .
+-Else if month is equal to 4, 6, 9 or 11 print “30 days”.
+-Else print “31 days”.
+*/
+
+function findDaysInMonth(){
+
+    //print the result in the html
+}
