5dd25a537c6868347c52adef8092cddec72dcfc0
juan sebastian correa <j.s.correa@videoamp.com>
2023-06-06 20:33:20 -0500

    exercises 1 & 2

---
+++ lesson-9/exercise-1/exercise-9.js
@@ -0,0 +1,9 @@
+/*
+Function `findLargest()` finds the largest of three number by using “>” and “&&” operator in JavaScript.
+
+*/
+
+function findLargest(){
+
+    //print in html the biggest
+}
---
+++ lesson-9/exercise-2/exercise-2.js
@@ -0,0 +1,9 @@
+/*
+Function `findTriangleType()` finds the type of the triangle (Equilateral, Isosceles, Scalene)
+ for given side values by using “==” and “&&” operator in JavaScript.
+*/
+
+function findTriangleType(){
+
+    //print in html the type of triangle
+}
