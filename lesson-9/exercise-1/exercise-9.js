f32de1502a6551e5016a2039b724e987319acda2
Juliana Montes <montesjuliana1989@gmail.com>
2023-06-12 17:43:16 -0500

    Juli exercise 1

--- lesson-9/exercise-1/exercise-9.js
+++ lesson-9/exercise-1/exercise-9.js
@@ -1,9 +1,24 @@
 /*
 Function `findLargest()` finds the largest of three number by using “>” and “&&” operator in JavaScript.
 
+
+
 */
 
-function findLargest(){
-
     //print in html the biggest
+
+
+let num1 = 10; // x
+let num2 = 15; // y
+let num3 = 20; // z
+
+function findLargest () {
+    if(num1 > num2 && num1 > num3) console.log("num1 = "+num1+" is the greatest");
+    else if(num3 > num1 && num3 > num2) console.log("num3 = "+num3+" is the greatest");
+    else console.log("Y = "+num2+" is the greatest");
+
 }
+
+findLargest ()
+
+
