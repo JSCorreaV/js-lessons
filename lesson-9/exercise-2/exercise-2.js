cef40c27d7eab21b954034a1d33c739794d360ca
Juliana Montes <montesjuliana1989@gmail.com>
2023-06-12 18:55:40 -0500

    juli second commit

--- lesson-9/exercise-2/exercise-2.js
+++ lesson-9/exercise-2/exercise-2.js
@@ -3,7 +3,27 @@ Function `findTriangleType()` finds the type of the triangle (Equilateral, Isosc
  for given side values by using “==” and “&&” operator in JavaScript.
 */
 
-function findTriangleType(){
+let side1 = 60
+let side2 = 60
+let side3 = 60
+
+function findTriangleType(side1, side2, side3){
+    if (side1 == side2 && side2 == side3 && side1 == side3){
+        console.log ("The triangle is equilateral");
+    }
+
+    else if (side1 == side2 && side2 != side3 && side1 != side3){
+        console.log ("The triangle is isosceles");
+    }
+    
+    else (side1 != side2 && side2 != side3 && side1 != side3); {
+        console.log ("The triangle is scalene");
+    }
+}
+
+findTriangleType ()
 
     //print in html the type of triangle
-}
+
+
+
