ea9677f4572213499b09dda14329d076a26fd7ec
Juliana Montes <montesjuliana1989@gmail.com>
2023-06-12 19:21:34 -0500

    Juli third commit

--- lesson-9/exercise-2/exercise-2.js
+++ lesson-9/exercise-2/exercise-2.js
@@ -10,14 +10,17 @@ let side3 = 60
 function findTriangleType(side1, side2, side3){
     if (side1 == side2 && side2 == side3 && side1 == side3){
         console.log ("The triangle is equilateral");
+        return "The triangle is equilateral";
     }
 
     else if (side1 == side2 && side2 != side3 && side1 != side3){
         console.log ("The triangle is isosceles");
+        return "The triangle is isosceles";
     }
     
-    else (side1 != side2 && side2 != side3 && side1 != side3); {
+    else (side1 != side2 && side2 != side3 && side1 != side3);{
         console.log ("The triangle is scalene");
+        return "The triangle is scalene";
     }
 }
 
