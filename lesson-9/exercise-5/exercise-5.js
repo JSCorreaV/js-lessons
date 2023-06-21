22275e407fb45dfe0350b8da391c00d742613583
juan sebastian correa <j.s.correa@videoamp.com>
2023-06-20 20:42:15 -0500

    lesson 10 + ex 7 - 8

--- lesson-9/exercise-5/exercise-5.js
+++ lesson-9/exercise-5/exercise-5.js
@@ -5,9 +5,29 @@ the highest
 the lowest
 */
 
-function listOfNumbers(){
-    list = [5,3,5,6,70,8,4];
+let result = "vacio";
+
+function listOfNumbers(list) {
+  let sum = 0;
+  let min = Number.MAX_VALUE;
+  let max = Number.MIN_VALUE;
+  let media;
+
+  for (let index = 0; index < list.length; index++) {
+    sum += list[index];
+
+    if (min > list[index]) {
+      min = list[index];
+    }
+
+    if (max < list[index]) {
+      max = list[index];
+    }
+  }
+  media = sum / list.length;
+  return `The results are: SUM = ${sum} MEDIA = ${media} MIN = ${min} MAX = ${max}`;
+  //return "The results are: SUM = " + sum + " MEDIA = " + media + " MIN = " + min + " MAX = " + max;
 }
-
-
-listOfNumbers()
+console.log(result);
+result = listOfNumbers([5, -33, 5, 6, 700, 8, 4, 54654, 76, 2, 5435]);
+console.log(result);
--- lesson-9/exercise-6/exercise-6.js
+++ lesson-9/exercise-6/exercise-6.js
@@ -1,9 +1,55 @@
 //Count all vowels in a given text
 
+/*function vowelCounter(cadenaDeTexto) {
+  counter = 0;
+  lista = [];
+  [...cadenaDeTexto].forEach((s) => lista.push(s)); //Op1
+  //lista = cadenaDeTexto.split(" "); OP2
 
-function vowelCounter(text){
-    [...text].forEach((s)=>console.log(s)); //this is a way of printing all caracters in a string, you have to find a way to store all the string values in an array
+
+  lista.forEach(letra => {
+    if (
+        letra == "a" ||
+        letra == "e" ||
+        letra == "i" ||
+        letra == "o" ||
+        letra == "u"
+    ) {
+        counter++;
+    }        
+  });
+
+    /*for (const letra of lista) {
+        if (
+            letra == "a" ||
+            letra == "e" ||
+            letra == "i" ||
+            letra == "o" ||
+            letra == "u"
+        ) {
+            counter++;
+        }    
+    }*/
+
+/*for (let index = 0; index < lista.length; index++) {
+    if (
+      lista[index] == "a" ||
+      lista[index] == "e" ||
+      lista[index] == "i" ||
+      lista[index] == "o" ||
+      lista[index] == "u"
+    ) {
+      counter++;
+    }
+  }
+
+  console.log(`Counter = ${counter}`);
 }
 
+vowelCounter("abce");*/
 
-vowelCounter("fkdlsajñriuweoñre")
+function vowelCounter(cadenaDeTexto) {
+  [...cadenaDeTexto].forEach((letra) => console.log(letra)); //this is a way of printing all caracters in a string, you have to find a way to store all the string values in an array
+}
+
+vowelCounter("abce");
