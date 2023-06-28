af8413fd18b5eba147293f681635f8fc04722daa
juan sebastian correa <j.s.correa@videoamp.com>
2023-06-27 23:21:36 -0500

    adding solution for ex 8

--- lesson-10/ex-7/ex-7.js
+++ lesson-10/ex-7/ex-7.js
@@ -1,11 +1,23 @@
 //Are two lists equals?
 
 function checkLists(listOne, listTwo) {
-  let equalLists = false;
+  let equalLists = true;
+  if (listOne.length != listTwo.length) {
+    equalLists = false;
+  } else {
+    for (let index = 0; index < listOne.length; index++) {
+      if (listOne[index] != listTwo[index]) {
+        equalLists = false;
+        break;
+      }
+    }
+  }
   return equalLists;
 }
 
-checkLists(["a", "b", "c"], ["a", "b", "c"]); //True
-checkLists(["a", "c", "b"], ["a", "b", "c"]); //False
-checkLists(["a", "b", "c", "d"], ["a", "b", "c"]); //False
-checkLists(["a", "b", "c"], ["a", "b", "c", "e"]); //False
+console.log(checkLists(["a", "b", "c"], ["a", "b"])); //false
+console.log(checkLists(["a", "b", "c"], ["c", "b", "c"])); //false
+console.log(checkLists(["a", "b", "c"], ["a", "b", "c"])); //True
+console.log(checkLists(["a", "c", "b"], ["a", "b", "c"])); //False
+console.log(checkLists(["a", "b", "c", "d"], ["a", "b", "c"])); //False
+console.log(checkLists(["a", "b", "c"], ["a", "b", "c", "e"])); //False
--- lesson-10/ex-8/ex-8.js
+++ lesson-10/ex-8/ex-8.js
@@ -1,12 +1,22 @@
 //Is this word a palindrome?
 
 function isAPalindrome(word) {
-  let isPalindrome = false;
-
+  let isPalindrome = true;
+  let wordArray = [];
+  [...word].forEach((caracter) => wordArray.push(caracter));
+  let j = wordArray.length - 1;
+  for (let i = 0; i < wordArray.length / 2; i++) {
+    if (wordArray[i] != wordArray[j]) {
+      isPalindrome = false;
+    }
+    j--;
+  }
   return isPalindrome;
 }
 
-isAPalindrome("civic"); //True
+console.log(isAPalindrome("civic")); //True
+console.log(isAPalindrome("civica")); //False
+console.log(isAPalindrome("civvic")); //True
 isAPalindrome("palindrome"); //False
 isAPalindrome("rotator"); //True
 isAPalindrome("word"); //False
