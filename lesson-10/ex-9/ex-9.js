9f5e034c7e3b190baf896e227da5515d686315fc
juan sebastian correa <j.s.correa@videoamp.com>
2023-06-27 23:08:06 -0500

    ex 9-10

---
+++ lesson-10/ex-9/ex-9.js
@@ -0,0 +1,12 @@
+//create an array with all odd numbers
+
+function findAllOddNumbers(numberList) {
+  oddList = [];
+
+  return oddList;
+}
+
+findAllOddNumbers([4, 5, 6, 7, 8, 9]); // result: [5,7,9]
+findAllOddNumbers([1, 2, 3, 4, 5]); // result: [1,3,5]
+findAllOddNumbers([1, 3, 5]); // result: [1,3,5]
+findAllOddNumbers([2, 4, 6, 8, 0]); //result: [])
