3cea4437c0808dd83f95c0e669f0cded577c4f10
Danielab0210 <dbm021096@gmail.com>
2023-07-15 17:29:44 -0500

    ex 11

--- lesson-10/ex-11/ex-11.js
+++ lesson-10/ex-11/ex-11.js
@@ -1,6 +1,7 @@
 // 
 /*find total price: 
 -if the bill is greater than 150,000 user should get 20% discount
+-if not the discount should be 10%
 -and the end of the bill we should show the total price and the discount if applies.
 */
 
@@ -18,16 +19,32 @@ const milk = 68000;
 const cereal = 15000;
 
 function totalPrice(products) {
-    let total = 0;
+    let total = 0; 
+    let totalWithDiscount = 0;
+    let discount = 0;
+    let today = new Date();
+    if(today.getDay() == 6){
+        console.log("+++++++++++++++++")
+        console.log(today.getDay())
+        discount = 0.1;
+    }
+
     for (let index = 0; index < products.length; index++) {
          total = products[index] + total;      
     } 
 
     if (total > 150000){
-        total = total * 0.80;
+        discount = discount + 0.2;
+        totalWithDiscount = total - (total * discount);
+        discountValue = total * discount; 
     }
-    
-    return total;
+    else {
+        discount = discount + 0.1;
+        totalWithDiscount = total - (total * discount);
+        discountValue = total * discount; 
+    }
+    console.log(`The total bill is: ${total} and the discount is: ${discountValue}, your new total with discount is ${totalWithDiscount}`)
+    return totalWithDiscount;
   }
 
   console.log(totalPrice([chocolate,milk,wine,dogChow])); //192000
